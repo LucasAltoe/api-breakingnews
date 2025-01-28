09cf5b1619e50040ec80c7d06ec948d2855e9814
Lucas Altoe <lucas.galc@gmail.com>
2025-01-28 12:51:50 -0300

    Implementing .env configuration

--- src/database/db.js
+++ src/database/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const connectDatabase = () => {
     console.log('Wait connecting to the database');
 
-    mongoose.connect("mongodb+srv://lucasgalc:root@cluster0.ftkzp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log('MongoDB Atlas Connected!'))
     .catch((err) => console.log(err));
 };
